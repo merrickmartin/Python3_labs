4b06384ccf579052c7b6b2bc72f7af568c7d0bc2
merrick martin <merrick.m.martin@gmail.com>
2024-11-18 20:52:11 +0000

    Created first.py

---
+++ first.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# author: Merrick Martin
+#version: 1.0
+#Description: This is our first script.
+name = input('what is your name: ')
+#print('My name is', name)  #comma parameter
+print("My name is " + name)
+import random
+print("My lucky number is", random.randint(1,50))
+import math
+print("Cosine of 0.5 is", math.cos(0.5))
